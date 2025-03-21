4cefc3b93650b4f2e33fec937ca93f56612ea32b
Luis Mario Cervantes Suarez <lmcervantessuarez@gmail.com>
2025-03-21 06:13:48 +0000

    Agregar actualización en tiempo real para las últimas transacciones y estadísticas de ingresos y gastos

--- admon/components/latest_transactions.py
+++ admon/components/latest_transactions.py
@@ -1,23 +1,34 @@
-from django_unicorn.components import UnicornView
+from django_unicorn.components import UnicornView,PollUpdate
 from admon.models import IngresoModel, GastoModel
 from itertools import chain
+from django.utils.timezone import now
 
 class LatestTransactionsView(UnicornView):
+    polling_disabled = False
     transacciones = []
     count = 0
 
     def mount(self):
+        """Carga inicial de transacciones al montar el componente."""
+        self.update_transactions()
+
+    def update_transactions(self):
+        """Carga y actualiza la lista de transacciones."""
         ingresos = list(IngresoModel.objects.all().order_by('-fecha')[:3])
         gastos = list(GastoModel.objects.all().order_by('-fecha')[:3])
-        # Agregamos un atributo personalizado para identificar el tipo
+
         for ingreso in ingresos:
             ingreso.tipo = "ingreso"
-        
+
         for gasto in gastos:
             gasto.tipo = "gasto"
-        # Unir las listas y ordenarlas por fecha
+
         self.transacciones = sorted(
             chain(ingresos, gastos), key=lambda x: x.fecha, reverse=True
         )
-        # Contar el total de transacciones
         self.count = len(self.transacciones)
+
+    def get_transactions(self):
+        """Método que se ejecuta cada 2 segundos para actualizar la vista."""
+        self.update_transactions()
+        return PollUpdate(timing=2000, disable=False, method="get_transactions")
--- admon/components/view_stats.py
+++ admon/components/view_stats.py
@@ -1,17 +1,19 @@
-from django_unicorn.components import UnicornView
+from django_unicorn.components import UnicornView, PollUpdate
 from admon.models import IngresoModel, GastoModel
 from datetime import datetime
 class ViewStatsView(UnicornView):
     ingreso = 0
     gasto = 0
     balance = 0
+    mes = datetime.now().month
     
     def mount(self):
-        mes = datetime.now().month
-        print(mes)
         # Obtener todos los ingresos y gastos del mes
-        ingresos = IngresoModel.objects.filter(fecha__month=mes)
-        gastos = GastoModel.objects.filter(fecha__month=mes)
+        pass
+    def update_stats(self):
+        # Actualizar los ingresos, gastos y balance
+        ingresos = IngresoModel.objects.filter(fecha__month=self.mes)
+        gastos = GastoModel.objects.filter(fecha__month=self.mes)
         self.ingreso = sum([ingreso.monto for ingreso in ingresos])
         self.gasto = sum([gasto.monto for gasto in gastos])
         self.balance = self.ingreso - self.gasto
@@ -19,3 +21,7 @@ class ViewStatsView(UnicornView):
         self.balance = "{:,}".format(self.balance)
         self.ingreso = "{:,}".format(self.ingreso)
         self.gasto = "{:,}".format(self.gasto)
+    
+    def get_stats(self):
+        self.update_stats()
+        return PollUpdate(timing=2000, disable=False, method="get_stats")
